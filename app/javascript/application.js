1ae138926239c8d8fb900def144e84b6144575df
jaythree <giljr.2009@gmail.com>
2025-04-11 18:31:16 -0400

    Fixing javascript and upload icons

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,61 +1,69 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.addEventListener("turbo:load", function () {
-  addCertificate();
-  submitEmployeeForm();
+
+$(document).on('turbo:load', function(e) {
+	addCertificate();
+	submitEmployeeForm();
 });
 
 function addCertificate() {
-  $(document).on('click', '.add-certicate', function (e) {
-    if ($('form .certi-box').length < 10) {
-      $('.certificate_list').append($('.certificate_fields.d-none').html());
-    } else {
-      toastr.error("A maximum of 10 certificates are only allowed!");
-    }
-    e.stopImmediatePropagation();
-  });
+	$('.add-certicate').on('click', function(e) {
+		if($('form .certi-box').length < 10) {
+			$('.certificate_list').append($('.certificate_fields.d-none').html());
+		} else {
+			toastr.error("A maximum of 10 certificates are only allowed!.")
+		}
+		removeCerticate();
+		e.stopImmediatePropagation();
+	});
+}
+
+function removeCerticate() {
+	$('.remove_certi').on('click', function(e) {
+		$(this).closest('.certi-box').remove();
+		e.stopImmediatePropagation();
+	});
 }
 
 function submitEmployeeForm() {
-  $(document).on('click', '.submit-emp-form', function (e) {
-    let foundCerti = ($('form .certi-box').length !== 0);
+	$('.submit-emp-form').on('click', function(e) {
 
-    if (!foundCerti) {
-      toastr.error("You need to add at least one certificate");
-      e.preventDefault();
-    }
+        var foundCerti = ($('form .certi-box').length != 0);
+            if(!foundCerti) {
+                toastr.error("You need to add atleast one certificate");
+                e.preventDefault();
+            }
 
-    if (foundCerti) {
-      $('form .certificate-file').each(function () {
-        if ($(this).val().length === 0) {
-          foundCerti = false;
-          return false;
-        }
-      });
+            if(foundCerti) {
+                $('form .certificate-file').each(function(index, element) {
+                    if($(this).val().length == 0) {
+                        foundCerti=false;
+                        return false;
+                    }
+                });
+        
+                if(!foundCerti) {
+                    toastr.error("Certificates Can't be blank");
+                    e.preventDefault();
+                }
+            }
 
-      if (!foundCerti) {
-        toastr.error("Certificates can't be blank");
-        e.preventDefault();
-      }
-    }
-
-    if (foundCerti) {
-      const photoExt = $("#photo").val().split('.').pop().toLowerCase();
-      const panExt = $("#employeeinfo_pan_card").val().split('.').pop().toLowerCase();
-      const allowed = ['png', 'jpeg', 'jpg'];
-
-      if (!allowed.includes(photoExt)) {
-        toastr.error("Photo must be png/jpeg/jpg only");
-        foundCerti = false;
-        e.preventDefault();
-      }
-
-      if (!allowed.includes(panExt)) {
-        toastr.error("Pan card must be png/jpeg/jpg only");
-        foundCerti = false;
-        e.preventDefault();
-      }
-    }
-  });
+            if(foundCerti) {
+                var photoFileExtension = $("#photo").val().split('.').pop().toLowerCase();
+                var panCardFileExtension = $("#employeeinfo_pan_card").val().split('.').pop().toLowerCase();
+                var allowedFileExtesion = ['png', 'jpeg', 'jpg']
+                if(!allowedFileExtesion.includes(photoFileExtension)){
+                    toastr.error("Photo must be png/jpeg/jpg only");
+                    foundCerti=false;
+                    e.preventDefault();
+                }
+            
+                if(foundCerti && !allowedFileExtesion.includes(panCardFileExtension)){
+                    toastr.error("Pan card must be png/jpeg/jpg only");
+                    foundCerti=false;
+                    e.preventDefault();
+                }
+                       }
+	});
 }
